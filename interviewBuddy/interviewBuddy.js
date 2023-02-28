de40e11fdd830bdd5093aa771bd16fe202a6c139
Josh Carvell <j_carvell@hotmail.co.uk>
2023-02-28 23:27:27 +0000

    Install JSON server to act as a RESTful API from JSON data

--- interviewBuddy/interviewBuddy.js
+++ interviewBuddy/interviewBuddy.js
@@ -4,10 +4,14 @@ class InterviewBuddy {
     this.startButton = document.getElementById(buttonID);
     this.appTimer = document.getElementById(timerEl)
     this.questionDisplay = document.getElementById(questionEl)
+    this.questionData = null
     console.log(this.questionDisplay);
   }
 
   async loadQuestions(){
-    
+    const response = await fetch('questions.json');
+    const data = response.json;
+    this.questionData = data;
+    console.log(this.questionData);
   }
 }
