a98da6b8f1c9f492ad932077764838771cffcbc5
Josh Carvell <j_carvell@hotmail.co.uk>
2023-02-28 22:56:26 +0000

    add reference to button element in class constuctor

--- interviewBuddy/interviewBuddy.js
+++ interviewBuddy/interviewBuddy.js
@@ -1,3 +1,9 @@
 class InterviewBuddy {
-  
+
+  constructor(buttonID) {
+    this.startButton = document.getElementById(buttonID);
+    console.log(this.startButton);
+    // this.appTimer = 
+    // this.questionDisplay = 
+  }
 }
